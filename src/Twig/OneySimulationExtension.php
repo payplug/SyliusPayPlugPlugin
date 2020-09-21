5c9723a8b8c792c96dff77296c6625b1c7a635b7
Jibé Barth <barth.jib@gmail.com>
2020-09-21 17:22:44 +0200

    Add cache around getOneySimulation

--- src/Twig/OneySimulationExtension.php
+++ src/Twig/OneySimulationExtension.php
@@ -7,7 +7,10 @@ namespace PayPlug\SyliusPayPlugPlugin\Twig;
 use Payplug\Exception\BadRequestException;
 use Payplug\OneySimulation;
 use PayPlug\SyliusPayPlugPlugin\ApiClient\PayPlugApiClientInterface;
+use Psr\Log\LoggerInterface;
 use Sylius\Component\Order\Context\CartContextInterface;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\Cache\ItemInterface;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
 
@@ -19,12 +22,22 @@ final class OneySimulationExtension extends AbstractExtension
     /** @var \PayPlug\SyliusPayPlugPlugin\ApiClient\PayPlugApiClientInterface */
     private $oneyClient;
 
+    /** @var \Symfony\Contracts\Cache\CacheInterface */
+    private $cache;
+
+    /** @var \Psr\Log\LoggerInterface */
+    private $payplugLogger;
+
     public function __construct(
         PayPlugApiClientInterface $oneyClient,
-        CartContextInterface $cartContext
+        CartContextInterface $cartContext,
+        CacheInterface $cache,
+        LoggerInterface $payplugLogger
     ) {
         $this->cartContext = $cartContext;
         $this->oneyClient = $oneyClient;
+        $this->cache = $cache;
+        $this->payplugLogger = $payplugLogger;
     }
 
     public function getFunctions(): array
@@ -38,29 +51,38 @@ final class OneySimulationExtension extends AbstractExtension
     {
         /** @var \Sylius\Component\Core\Model\Order $currentCart */
         $currentCart = $this->cartContext->getCart();
-        $data = [
-            'amount' => $currentCart->getTotal(),
-            'country' => 'FR', // TODO retrieve country
-            'operations' => [
-                'x3_with_fees',
-                'x4_with_fees',
-            ],
-        ];
+        $country = \explode('_', $currentCart->getLocaleCode() ?? 'fr_FR')[1];
+        $cacheKey = \sprintf('oney_simulation_%s_%s', $country, $currentCart->getTotal());
 
-        try {
-            $currency = $currentCart->getCurrencyCode();
-            $accountData = $this->oneyClient->getAccount();
-            $simulationData = OneySimulation::getSimulations($data, $this->oneyClient->getConfiguration());
-
-            return \array_merge(
-                [
-                    'min_amount' => $accountData['configuration']['oney']['min_amounts'][$currency],
-                    'max_amount' => $accountData['configuration']['oney']['max_amounts'][$currency],
+        return $this->cache->get($cacheKey, function (ItemInterface $item) use ($currentCart, $country): array {
+            $item->expiresAfter(new \DateInterval('P1D')); // One day of cache
+            $data = [
+                'amount' => $currentCart->getTotal(),
+                'country' => $country,
+                'operations' => [
+                    'x3_with_fees',
+                    'x4_with_fees',
                 ],
-                $simulationData
-            );
-        } catch (BadRequestException $exception) {
-            return [];
-        }
+            ];
+            $this->payplugLogger->debug('[PayPlug] Call oney simulation with following data', $data);
+
+            try {
+                $currency = $currentCart->getCurrencyCode();
+                $accountData = $this->oneyClient->getAccount();
+                $simulationData = OneySimulation::getSimulations($data, $this->oneyClient->getConfiguration());
+
+                $this->payplugLogger->debug('[PayPlug] Oney simulation response', $simulationData);
+
+                return \array_merge(
+                    [
+                        'min_amount' => $accountData['configuration']['oney']['min_amounts'][$currency],
+                        'max_amount' => $accountData['configuration']['oney']['max_amounts'][$currency],
+                    ],
+                    $simulationData
+                );
+            } catch (BadRequestException $exception) {
+                return [];
+            }
+        });
     }
 }
