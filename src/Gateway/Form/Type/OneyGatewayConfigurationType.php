5e8edd0c4d62415cbc24b624b435d26e3b312fc4
Jibé Barth <barth.jib@gmail.com>
2020-11-11 11:16:08 +0100

    Coding standards

--- src/Gateway/Form/Type/OneyGatewayConfigurationType.php
+++ src/Gateway/Form/Type/OneyGatewayConfigurationType.php
@@ -9,11 +9,9 @@ use PayPlug\SyliusPayPlugPlugin\Gateway\Validator\Constraints\IsPayPlugSecretKey
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\NotBlank;
-use Symfony\Component\Validator\Constraints\Url;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
 final class OneyGatewayConfigurationType extends AbstractType
