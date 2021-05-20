c6e60878d37f8d3fb2ebab6d8deedc99cc7c445f
Daniel Rybakov <drybakov@payplug.com>
2021-05-20 14:02:56 +0200

    Try fix behat secret key empty

--- tests/Behat/Context/Ui/Admin/ManagingPaymentMethodContext.php
+++ tests/Behat/Context/Ui/Admin/ManagingPaymentMethodContext.php
@@ -28,7 +28,7 @@ final class ManagingPaymentMethodContext implements Context
     }
 
     /**
-     * @Then I should be notified that :fields cannot be empty.
+     * @Then I should be notified that :fields cannot be empty
      */
     public function iShouldBeNotifiedThatCannotBeBlank(string $fields): void
     {
@@ -36,8 +36,8 @@ final class ManagingPaymentMethodContext implements Context
 
         foreach ($fields as $field) {
             Assert::true($this->createPage->containsErrorWithMessage(sprintf(
-                '%s cannot be blank.',
-                trim($field)
+                'The %s cannot be empty.',
+                strtolower(trim($field))
             )));
         }
     }
