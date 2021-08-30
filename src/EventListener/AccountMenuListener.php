c7d369d183a94e9a4febf2d73406ff835f4ff02f
Trinh_Vo <trinh.vo@synolia.com>
2021-08-30 19:33:59 +0200

    SL-10 show saved cards info on account

---
+++ src/EventListener/AccountMenuListener.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PayPlug\SyliusPayPlugPlugin\EventListener;
+
+use Sylius\Bundle\UiBundle\Menu\Event\MenuBuilderEvent;
+use Sylius\Component\Core\Model\CustomerInterface;
+use Sylius\Component\Customer\Context\CustomerContextInterface;
+
+final class AccountMenuListener
+{
+    public function addAccountMenuItems(MenuBuilderEvent $event): void
+    {
+        $menu = $event->getMenu();
+
+        $menu->addChild('card', [
+            'route' => 'payplug_sylius_card_account_index',
+        ])
+            ->setAttribute('type', 'link')
+            ->setLabel('payplug_sylius_payplug_plugin.ui.account.saved_cards.title')
+            ->setLabelAttribute('icon', 'credit card')
+        ;
+    }
+}
