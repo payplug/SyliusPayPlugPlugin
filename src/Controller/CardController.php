a97440d9866440bfc5a54d098bd13de37243d990
Trinh_Vo <trinh.vo@synolia.com>
2021-08-31 10:57:57 +0200

    SL-11-delete-saved-cards

--- src/Controller/CardController.php
+++ src/Controller/CardController.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace PayPlug\SyliusPayPlugPlugin\Controller;
 
+use Payplug\Exception\NotFoundException;
+use PayPlug\SyliusPayPlugPlugin\ApiClient\PayPlugApiClientInterface;
+use PayPlug\SyliusPayPlugPlugin\Entity\Card;
 use Sylius\Bundle\ResourceBundle\Doctrine\ORM\EntityRepository;
 use Sylius\Component\Core\Model\CustomerInterface;
 use Sylius\Component\Customer\Context\CustomerContextInterface;
@@ -30,17 +33,23 @@ final class CardController extends AbstractController
      * @var TranslatorInterface
      */
     private $translator;
+    /**
+     * @var PayPlugApiClientInterface
+     */
+    private $payPlugApiClient;
 
     public function __construct(
         CustomerContextInterface $customerContext,
         EntityRepository $payplugCardRepository,
         FlashBagInterface $flashBag,
-        TranslatorInterface $translator
+        TranslatorInterface $translator,
+        PayPlugApiClientInterface $payPlugApiClient
     ) {
         $this->customerContext = $customerContext;
         $this->payplugCardRepository = $payplugCardRepository;
         $this->flashBag = $flashBag;
         $this->translator = $translator;
+        $this->payPlugApiClient = $payPlugApiClient;
     }
 
     public function indexAction(): Response
@@ -55,12 +64,63 @@ final class CardController extends AbstractController
 
         $savedCards = $this->payplugCardRepository->findBy(['customer' => $customer], ['id' => 'DESC']);
 
-        return $this->render('@PayPlugSyliusPayPlugPlugin/card/index.html.twig', ['savedCards' => $savedCards]);
+        return $this->render('@PayPlugSyliusPayPlugPlugin/card/index.html.twig', [
+            'savedCards' => $savedCards,
+        ]);
     }
 
     public function deleteAction(int $id): Response
     {
-        //TODO
+        $customer = $this->customerContext->getCustomer();
+
+        if (!$customer instanceof CustomerInterface) {
+            return $this->redirectToRoute('sylius_shop_login');
+        }
+
+        $card = $this->payplugCardRepository->findOneBy(['id' => $id, 'customer' => $customer]);
+
+        if (!$card instanceof Card) {
+            return $this->redirectToRoute('payplug_sylius_card_account_index');
+        }
+
+        if (true === $this->isCardExpired($card)) {
+            $this->flashBag->add('error', $this->translator->trans('payplug_sylius_payplug_plugin.ui.account.saved_cards.deleted_error'));
+
+            return $this->redirectToRoute('payplug_sylius_card_account_index');
+        }
+
+        $cardToken = $card->getExternalId();
+
+        try {
+            $this->payPlugApiClient->deleteCard($cardToken);
+        } catch (NotFoundException $e) {
+            $this->flashBag->add('error', $this->translator->trans('payplug_sylius_payplug_plugin.ui.account.saved_cards.deleted_error'));
+
+            return $this->redirectToRoute('payplug_sylius_card_account_index');
+        }
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->remove($card);
+        $entityManager->flush();
+
+        $this->flashBag->add('success', $this->translator->trans('sylius.ui.deleted'));
+
         return $this->redirectToRoute('payplug_sylius_card_account_index');
+    }
+
+    private function isCardExpired(Card $card): bool
+    {
+        $now = new \DateTime();
+        $currentYear = $now->format("Y");
+        $expirationYear = (string) $card->getExpirationYear();
+
+        if (
+            ($currentYear < $expirationYear) ||
+            ($currentYear === $expirationYear && $now->format("n") <= (string) $card->getExpirationMonth())
+        ){
+            return false;
+        }
+
+        return true;
     }
 }
