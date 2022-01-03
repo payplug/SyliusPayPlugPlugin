88b415ea0bb294c6118ece17fdee39c2042b7d17
Nicolas MELONI <nicolas.meloni@synolia.com>
2022-01-03 09:54:17 +0100

    fix oney cached key

--- src/Provider/OneySimulation/CachedSimulationDataProvider.php
+++ src/Provider/OneySimulation/CachedSimulationDataProvider.php
@@ -4,28 +4,38 @@ declare(strict_types=1);
 
 namespace PayPlug\SyliusPayPlugPlugin\Provider\OneySimulation;
 
+use PayPlug\SyliusPayPlugPlugin\Provider\OneySupportedPaymentChoiceProvider;
 use Sylius\Component\Core\Model\OrderInterface;
 use Symfony\Contracts\Cache\CacheInterface;
 use Symfony\Contracts\Cache\ItemInterface;
 
 final class CachedSimulationDataProvider implements OneySimulationDataProviderInterface
 {
-    /** @var OneySimulationDataProviderInterface */
-    private $decorated;
+    private OneySimulationDataProviderInterface $decorated;
 
-    /** @var \Symfony\Contracts\Cache\CacheInterface */
-    private $cache;
+    private CacheInterface $cache;
 
-    public function __construct(OneySimulationDataProviderInterface $decorated, CacheInterface $cache)
-    {
+    private OneySupportedPaymentChoiceProvider $oneySupportedPaymentChoiceProvider;
+
+    public function __construct(
+        OneySimulationDataProviderInterface $decorated,
+        CacheInterface $cache,
+        OneySupportedPaymentChoiceProvider $oneySupportedPaymentChoiceProvider
+    ) {
         $this->decorated = $decorated;
         $this->cache = $cache;
+        $this->oneySupportedPaymentChoiceProvider = $oneySupportedPaymentChoiceProvider;
     }
 
     public function getForCart(OrderInterface $cart): array
     {
         $country = \explode('_', $cart->getLocaleCode() ?? 'fr_FR')[1];
-        $cacheKey = \sprintf('oney_simulation_%s_%s', $country, $cart->getTotal());
+        $cacheKey = \sprintf(
+            'oney_simulation_%s_%s_%s',
+            $country,
+            $cart->getTotal(),
+            $this->oneySupportedPaymentChoiceProvider->getFeesFor(),
+        );
 
         return $this->cache->get($cacheKey, function (ItemInterface $item) use ($cart): array {
             $item->expiresAfter(new \DateInterval('P1D')); // One day of cache
