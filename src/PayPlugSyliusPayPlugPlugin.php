2bef5e660a405d033b82db135a0ca3b117c7d192
Daniel Rybakov <drybakov@payplug.com>
2022-02-23 08:31:30 +0100

    Bump to 1.4.0

--- src/PayPlugSyliusPayPlugPlugin.php
+++ src/PayPlugSyliusPayPlugPlugin.php
@@ -9,7 +9,7 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 final class PayPlugSyliusPayPlugPlugin extends Bundle
 {
-    public const VERSION = '1.3.0';
+    public const VERSION = '1.4.0';
 
     use SyliusPluginTrait;
 }
