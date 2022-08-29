5cf7be5c54dd6d0923df75cd97d4fd2b45798fec
Trinh_Vo <trinh.vo@synolia.com>
2022-08-29 16:07:59 +0200

    SYL-170 add validator for constraint can not save bancontact method if key is not live or no access

--- src/Gateway/Validator/Constraints/IsCanSaveBancontactMethod.php
+++ src/Gateway/Validator/Constraints/IsCanSaveBancontactMethod.php
@@ -14,6 +14,9 @@ final class IsCanSaveBancontactMethod extends Constraint
     /** @var string */
     public $noTestKeyMessage = 'payplug_sylius_payplug_plugin.bancontact.can_not_save_method_with_test_key';
 
+    /** @var string */
+    public $noAccessMessage = 'payplug_sylius_payplug_plugin.bancontact.can_not_save_method_no_access';
+
     public function validatedBy(): string
     {
         return IsCanSaveBancontactMethodValidator::class;
--- src/Gateway/Validator/Constraints/IsCanSaveBancontactMethodValidator.php
+++ src/Gateway/Validator/Constraints/IsCanSaveBancontactMethodValidator.php
@@ -5,6 +5,9 @@ declare(strict_types=1);
 namespace PayPlug\SyliusPayPlugPlugin\Gateway\Validator\Constraints;
 
 use Payplug\Exception\UnauthorizedException;
+use PayPlug\SyliusPayPlugPlugin\ApiClient\PayPlugApiClientFactory;
+use PayPlug\SyliusPayPlugPlugin\Checker\CanSaveBancontactMethodChecker;
+use PayPlug\SyliusPayPlugPlugin\Gateway\BancontactGatewayFactory;
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
 use Symfony\Component\Validator\Exception\UnexpectedTypeException;
@@ -15,6 +18,14 @@ use Symfony\Component\Validator\Exception\UnexpectedValueException;
  */
 final class IsCanSaveBancontactMethodValidator extends ConstraintValidator
 {
+    /** @var PayPlugApiClientFactory */
+    private $apiClientFactory;
+
+    public function __construct(PayPlugApiClientFactory $apiClientFactory)
+    {
+        $this->apiClientFactory = $apiClientFactory;
+    }
+
     public function validate($value, Constraint $constraint): void
     {
         if (!$constraint instanceof IsCanSaveBancontactMethod) {
@@ -29,8 +40,22 @@ final class IsCanSaveBancontactMethodValidator extends ConstraintValidator
 
         $secretKey = $this->context->getRoot()->getData()->getGatewayConfig()->getConfig()['secretKey'];
 
-        if (strpos($secretKey, 'test') !== false) {
-            $this->context->buildViolation($constraint->noTestKeyMessage)->addViolation();
+        $checker = new CanSaveBancontactMethodChecker($this->apiClientFactory->create(BancontactGatewayFactory::FACTORY_NAME, $secretKey));
+
+        try {
+            if (!$checker->isLive()) {
+                $this->context->buildViolation($constraint->noTestKeyMessage)->addViolation();
+
+                return;
+            }
+
+            if (!$checker->isEnabled()) {
+                $this->context->buildViolation($constraint->noAccessMessage)->addViolation();
+            }
+
+            return;
+        } catch (UnauthorizedException|\LogicException $exception) {
+            return;
         }
     }
 }
