b69dc6be9a3444d448531ee8daefb61d76df9d39
Trinh_Vo <trinh.vo@synolia.com>
2023-01-05 14:43:21 +0100

    SYL-181 catch HttpException in AbortPaymentProcessor

--- src/PaymentProcessing/AbortPaymentProcessor.php
+++ src/PaymentProcessing/AbortPaymentProcessor.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace PayPlug\SyliusPayPlugPlugin\PaymentProcessing;
 
-use Payplug\Exception\ForbiddenException;
+use Payplug\Exception\HttpException;
 use PayPlug\SyliusPayPlugPlugin\ApiClient\PayPlugApiClientInterface;
 use Sylius\Component\Core\Model\PaymentInterface;
 
@@ -25,7 +25,7 @@ class AbortPaymentProcessor
             // and go to the order history in another tab to click on pay again, then fail the transaction
             // and go back on the first PayPlug payment page and succeed it, it stays failed as its first payment model is already failed
             $this->payPlugApiClient->abortPayment($payment->getDetails()['payment_id']);
-        } catch (ForbiddenException $forbiddenException) {
+        } catch (HttpException $httpException) {
         }
     }
 }
