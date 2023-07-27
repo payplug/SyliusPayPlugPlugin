daecf223e12bb2647e5fdc92cb7e28d21e5a5639
Nicolas MELONI <nicolas.meloni@synolia.com>
2023-07-27 14:56:41 +0200

    fix SYLIUS-248 add PHP 8 ORM attributes

--- src/Entity/Traits/PaymentTrait.php
+++ src/Entity/Traits/PaymentTrait.php
@@ -4,11 +4,14 @@ declare(strict_types=1);
 
 namespace PayPlug\SyliusPayPlugPlugin\Entity\Traits;
 
+use Doctrine\ORM\Mapping as ORM;
 use PayPlug\SyliusPayPlugPlugin\Entity\RefundHistory;
 
 trait PaymentTrait
 {
-    /** @ORM\OneToMany(targetEntity=RefundHistory::class, mappedBy="payment", orphanRemoval=true) */
+    /**
+     * @ORM\OneToMany(targetEntity=RefundHistory::class, mappedBy="payment", orphanRemoval=true)
+     */
     #[ORM\OneToMany(targetEntity: RefundHistory::class, mappedBy: 'payment', orphanRemoval: true)]
     protected $refundHistories;
 }
