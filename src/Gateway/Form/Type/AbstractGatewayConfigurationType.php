2f1dc93bc13f3eb0145d11b77e03ff72038ee360
Nicolas MELONI <nicolas.meloni@synolia.com>
2024-06-18 08:58:46 +0200

    SYL-220 - set payplug key as password

--- src/Gateway/Form/Type/AbstractGatewayConfigurationType.php
+++ src/Gateway/Form/Type/AbstractGatewayConfigurationType.php
@@ -12,7 +12,7 @@ use Sylius\Bundle\PayumBundle\Model\GatewayConfigInterface;
 use Sylius\Component\Core\Model\ChannelInterface;
 use Sylius\Component\Resource\Repository\RepositoryInterface;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormError;
 use Symfony\Component\Form\FormEvent;
@@ -58,7 +58,7 @@ class AbstractGatewayConfigurationType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('secretKey', TextType::class, [
+            ->add('secretKey', PasswordType::class, [
                 'label' => 'payplug_sylius_payplug_plugin.ui.secret_key',
                 'validation_groups' => self::VALIDATION_GROUPS,
                 'constraints' => [
