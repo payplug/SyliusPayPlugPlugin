4dc16e08fea284160117f1f9ff57677cbdd44416
Jibé Barth <barth.jib@gmail.com>
2024-11-21 15:52:58 +0100

    PPSYL-98 - Add new checkbox to enable payment integrated

--- src/Gateway/PayPlugGatewayFactory.php
+++ src/Gateway/PayPlugGatewayFactory.php
@@ -7,10 +7,11 @@ namespace PayPlug\SyliusPayPlugPlugin\Gateway;
 final class PayPlugGatewayFactory extends AbstractGatewayFactory
 {
     public const FACTORY_NAME = 'payplug';
-
     public const FACTORY_TITLE = 'PayPlug';
 
+    // Custom gateway configuration keys
     public const ONE_CLICK = 'oneClick';
+    public const INTEGRATED_PAYMENT = 'integratedPayment';
 
     public const AUTHORIZED_CURRENCIES = [
         'EUR' => [
