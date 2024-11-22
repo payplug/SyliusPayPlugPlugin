a98d372cfe378cbb301b16fdd89f6e056df9efc0
Jibé Barth <barth.jib@gmail.com>
2024-11-22 10:11:03 +0100

    PPSYL-101 - Add is_payplug_live twig function
    Call {{ is_payplug_live(paymentMethod) }} to know if a given payment method is configured as live or not for a payplug one. Useful later while loading iframe for integrated payment

--- src/Twig/PayPlugExtension.php
+++ src/Twig/PayPlugExtension.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace PayPlug\SyliusPayPlugPlugin\Twig;
 
+use PayPlug\SyliusPayPlugPlugin\ApiClient\PayPlugApiClientFactory;
 use PayPlug\SyliusPayPlugPlugin\Checker\CanSaveCardCheckerInterface;
 use Sylius\Component\Core\Model\PaymentMethodInterface;
 use Twig\Extension\AbstractExtension;
@@ -13,21 +14,33 @@ final class PayPlugExtension extends AbstractExtension
 {
     /** @var CanSaveCardCheckerInterface */
     private $canSaveCardChecker;
+    private PayPlugApiClientFactory $apiClientFactory;
 
-    public function __construct(CanSaveCardCheckerInterface $canSaveCardChecker)
-    {
+    public function __construct(
+        CanSaveCardCheckerInterface $canSaveCardChecker,
+        PayPlugApiClientFactory $apiClientFactory,
+    ) {
         $this->canSaveCardChecker = $canSaveCardChecker;
+        $this->apiClientFactory = $apiClientFactory;
     }
 
     public function getFunctions(): array
     {
         return [
             new TwigFunction('is_save_card_enabled', [$this, 'isSaveCardAllowed']),
+            new TwigFunction('is_payplug_live', [$this, 'isLive']),
         ];
     }
 
     public function isSaveCardAllowed(PaymentMethodInterface $paymentMethod): bool
     {
         return $this->canSaveCardChecker->isAllowed($paymentMethod);
+    }
+
+    public function isLive(PaymentMethodInterface $paymentMethod): bool
+    {
+        $client = $this->apiClientFactory->createForPaymentMethod($paymentMethod);
+
+        return (bool) ($client->getAccount()['is_live']);
     }
 }
