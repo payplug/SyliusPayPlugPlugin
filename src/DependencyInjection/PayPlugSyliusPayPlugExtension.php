114cf7ef4d767394183885451149ef5706a03c50
Jibé Barth <barth.jib@gmail.com>
2025-04-28 14:30:01 +0200

    chore(di): separate prepend twig specific
    And remove override of Sylius Shop/Admin templates

--- src/DependencyInjection/PayPlugSyliusPayPlugExtension.php
+++ src/DependencyInjection/PayPlugSyliusPayPlugExtension.php
@@ -25,63 +25,31 @@ final class PayPlugSyliusPayPlugExtension extends Extension implements PrependEx
         $xmlloader = new XmlFileLoader($container, new FileLocator(dirname(__DIR__, 2) . '/config/services'));
 
         $ymlloader->load('services.yaml');
-
+        // TODO: migrate to YAML
         $xmlloader->load('client.xml');
         $xmlloader->load('gateway.xml');
     }
 
     public function prepend(ContainerBuilder $container): void
     {
+        $this->prependTwigExtension($container);
+        $this->prependDoctrineMigrations($container);
+    }
+
+    private function prependTwigExtension(ContainerBuilder $container): void
+    {
         if (!$container->hasExtension('twig')) {
             return;
         }
 
-        $viewsPath = dirname(__DIR__, 2) . '/templates/';
-        // This adds our override in twig paths with correct namespace if there are not already overridden
-        // No need for the final user to copy it
-        $paths = [
-            $viewsPath . 'SyliusShopBundle' => 'SyliusShop',
-            $viewsPath . 'SyliusAdminBundle' => 'SyliusAdmin',
-            $viewsPath . 'SyliusUiBundle' => 'SyliusUi',
-        ];
-
-        $twigConfig = $container->getExtensionConfig('twig');
-
-        foreach ($paths as $key => $path) {
-            if ($this->isPathAlreadyInConfiguration($path, $twigConfig)) {
-                unset($paths[$key]);
-            }
-        }
-
+        // TODO: check if still mandatory on v2
         $container->prependExtensionConfig('twig', [
-            'paths' => $paths,
             'form_themes' => [
                 '@PayPlugSyliusPayPlugPlugin/form/form_gateway_config_row.html.twig',
                 '@PayPlugSyliusPayPlugPlugin/form/sylius_checkout_select_payment_row.html.twig',
                 '@PayPlugSyliusPayPlugPlugin/form/complete_info_popin.html.twig',
             ],
         ]);
-
-        $this->prependDoctrineMigrations($container);
-    }
-
-    /**
-     * Verify if a given namespace is already extended
-     *
-     * @param string $namespace The namespace to verify
-     * @param array $configurations The given configurations
-     */
-    protected function isPathAlreadyInConfiguration(string $namespace, array $configurations): bool
-    {
-        foreach ($configurations as $configuration) {
-            foreach ($configuration as $parameter => $values) {
-                if ('paths' === $parameter && in_array($namespace, $values, true)) {
-                    return true;
-                }
-            }
-        }
-
-        return false;
     }
 
     protected function getMigrationsNamespace(): string
