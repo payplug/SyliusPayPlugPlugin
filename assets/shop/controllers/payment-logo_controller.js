764b0997ede3030490eaf2e89dbbfebbe1e9bb69
Thomas VERMOT-GAUCHY <thomas.vermot-gauchy@synolia.com>
2025-04-30 11:52:27 +0200

    PPSYL-136 - sylius v2 - Checkout payment - Bancontact integration (with AmericanExpress mutualization)

--- assets/shop/controllers/payment-logo_controller.js
+++ assets/shop/controllers/payment-logo_controller.js
@@ -5,10 +5,15 @@ export default class extends Controller {
   static values = {
     inputId: String,
     logoUrl: String,
+    className: String,
   };
   connect() {
+    if (!this.inputIdValue || !this.logoUrlValue) {
+      return;
+    }
+
     const label = document.querySelector(`[for="${this.inputIdValue}"]`);
     label.style.setProperty('--logo', `url(${this.logoUrlValue})`);
-    label.classList.add('american-express-label');
+    label.classList.add('payment-label-with-image', this.classNameValue);
   }
 }
