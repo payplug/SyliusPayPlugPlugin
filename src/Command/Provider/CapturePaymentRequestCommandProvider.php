85c5657091ecf278c26332131499b3ceedb26f1f
Jibé Barth <barth.jib@gmail.com>
2025-06-01 18:36:30 +0200

    PPSYL-138 - IntegratedPayment - do not dispatch CapturePaymentRequest that could redirect to payplug

--- src/Command/Provider/CapturePaymentRequestCommandProvider.php
+++ src/Command/Provider/CapturePaymentRequestCommandProvider.php
@@ -2,7 +2,9 @@
 
 namespace PayPlug\SyliusPayPlugPlugin\Command\Provider;
 
+use PayPlug\SyliusPayPlugPlugin\ApiClient\PayPlugApiClientInterface;
 use PayPlug\SyliusPayPlugPlugin\Command\CapturePaymentRequest;
+use Sylius\Bundle\PaymentBundle\Command\Offline\CapturePaymentRequest as OfflineCapturePaymentRequest;
 use Sylius\Bundle\PaymentBundle\CommandProvider\PaymentRequestCommandProviderInterface;
 use Sylius\Component\Payment\Model\PaymentRequestInterface;
 use Symfony\Component\DependencyInjection\Attribute\AutoconfigureTag;
@@ -36,6 +38,35 @@ final class CapturePaymentRequestCommandProvider implements PaymentRequestComman
 
     public function provide(PaymentRequestInterface $paymentRequest): object
     {
+        $payment = $paymentRequest->getPayment();
+        $details = $payment->getDetails();
+
+        if ($this->isAlreadyCreated($paymentRequest)) {
+            // The payment has already been created, let's use the offline capture request to be redirected to the thank-you page
+            // TODO: use our own AlreadyPaidCapturePaymentRequest class
+            return new OfflineCapturePaymentRequest($paymentRequest->getId());
+        }
+
         return new CapturePaymentRequest($paymentRequest->getId());
+    }
+
+    private function isAlreadyCreated(PaymentRequestInterface $paymentRequest): bool
+    {
+        $payment = $paymentRequest->getPayment();
+        $details = $payment->getDetails();
+
+        if (
+            isset($details['status'], $details['payment_id']) &&
+            PayPlugApiClientInterface::STATUS_CREATED !== $details['status']
+        ) {
+            return true;
+        }
+
+        if (
+            array_key_exists('status', $details) &&
+            PayPlugApiClientInterface::STATUS_CAPTURED === $details['status']
+        ) {
+            return true;
+        }
     }
 }
