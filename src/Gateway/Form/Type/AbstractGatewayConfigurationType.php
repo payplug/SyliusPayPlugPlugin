4c17f726b792433bf2716eb4c7d5ff640e82f8be
Jibé Barth <barth.jib@gmail.com>
2025-08-29 09:25:30 +0200

    PPSYL-163 - Replace secretKey by live checkbox

--- src/Gateway/Form/Type/AbstractGatewayConfigurationType.php
+++ src/Gateway/Form/Type/AbstractGatewayConfigurationType.php
@@ -13,7 +13,9 @@ use Sylius\Bundle\PayumBundle\Model\GatewayConfigInterface;
 use Sylius\Component\Core\Model\ChannelInterface;
 use Sylius\Component\Resource\Repository\RepositoryInterface;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormError;
 use Symfony\Component\Form\FormEvent;
@@ -47,7 +49,14 @@ class AbstractGatewayConfigurationType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('secretKey', PasswordType::class, [
+            ->add('live', CheckboxType::class, [
+                'block_name' => 'payplug_checkbox',
+                'label' => 'payplug_sylius_payplug_plugin.ui.live',
+                'help' => 'payplug_sylius_payplug_plugin.ui.live_help',
+                'help_html' => true,
+                'required' => false,
+            ])
+            ->add('secretKey', TextType::class, [
                 'label' => 'payplug_sylius_payplug_plugin.ui.secret_key',
                 'validation_groups' => self::VALIDATION_GROUPS,
                 'constraints' => [
