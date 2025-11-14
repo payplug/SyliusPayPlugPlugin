c31d574c90dc86982e5f048957030fc1976090ea
Daniel FLOREZ MURILLO <dmurillo@payplug.com>
2025-11-14 14:42:58 +0100

    chore: fix version

--- src/PayPlugSyliusPayPlugPlugin.php
+++ src/PayPlugSyliusPayPlugPlugin.php
@@ -9,7 +9,12 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 final class PayPlugSyliusPayPlugPlugin extends Bundle
 {
-    public const VERSION = '1.11.1';
-
     use SyliusPluginTrait;
+
+    public const VERSION = '2.0.0';
+
+    public function getPath(): string
+    {
+        return \dirname(__DIR__);
+    }
 }
